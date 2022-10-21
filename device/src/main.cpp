c25ba63a2624442e48820215af15d5352da04620
arnoson <omasocke@hotmail.de>
2022-10-21 16:10:41 +0200

    feat: add simple node/browser dev

--- device/src/main.cpp
+++ device/src/main.cpp
@@ -4,6 +4,9 @@
 #include <SPI.h>
 #include <SlipSerial.h>
 
+using Bridge::Data;
+using Bridge::RequestId;
+
 SlipSerial serial(Serial);
 
 void setup() {
@@ -13,6 +16,13 @@ void setup() {
 
   Bridge::begin(serial);
   FileSystem::begin();
+
+  Bridge::addMethod("/echo/int", [](Data &data) {
+    RequestId id = data.getInt(0);
+    if (!Bridge::validateData(data, "ii", 2)) return Bridge::respondError(id);
+    auto number = data.getInt(1);
+    Bridge::respond(id, number);
+  });
 }
 
 void loop() { Bridge::update(); }
